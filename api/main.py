ee450bd5f7f18d967d70f118e1522ede202302ee
perforb <dev.perfumed.garden@gmail.com>
2023-07-07 19:15:20 +0900

    Add some endpoints

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from api.routers import task, done
 
+app = FastAPI(
+    title="ToDo API",
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(task.router)
+app.include_router(done.router)
