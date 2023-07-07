e5da6a336fe53aaf344d9ec7f8cb21568a953572
perforb <dev.perfumed.garden@gmail.com>
2023-07-07 19:52:29 +0900

    Update responses

---
+++ api/schemas/task.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+
+
+class TaskBase(BaseModel):
+    title: str | None = Field(None, example="クリーニングを取りに行く")
+
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, example="完了フラグ")
+
+    class Config:
+        orm_mode = True
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskBase):
+    id: int
+
+    class Config:
+        orm_mode = True
