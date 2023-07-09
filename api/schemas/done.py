f9a3ac269b8479719e857efeb3c3b880647c9ef4
perforb <dev.perfumed.garden@gmail.com>
2023-07-09 16:24:23 +0900

    Upgrade to Pydantic v2

--- api/schemas/done.py
+++ api/schemas/done.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel
 class DoneResponse(BaseModel):
     id: int
 
-    class Config:
-        orm_mode = True
+    class ConfigDict:
+        from_attributes = True
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -4,16 +4,16 @@ from pydantic import BaseModel, Field
 
 
 class TaskBase(BaseModel):
-    title: str | None = Field(None, example="クリーニングを取りに行く")
-    due_date: datetime.date | None = Field(None, example="2024-12-01")
+    title: str | None = Field(None, json_schema_extra={"example": "クリーニングを取りに行く"})
+    due_date: datetime.date | None = Field(None, json_schema_extra={"example": "2024-12-01"})
 
 
 class Task(TaskBase):
     id: int
-    done: bool = Field(False, example="完了フラグ")
+    done: bool = Field(False, json_schema_extra={"example": "完了フラグ"})
 
-    class Config:
-        orm_mode = True
+    class ConfigDict:
+        from_attributes = True
 
 
 class TaskCreate(TaskBase):
@@ -23,5 +23,5 @@ class TaskCreate(TaskBase):
 class TaskCreateResponse(TaskBase):
     id: int
 
-    class Config:
-        orm_mode = True
+    class ConfigDict:
+        from_attributes = True
